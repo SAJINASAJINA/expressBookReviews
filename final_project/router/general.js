006f5b6483e9b08387a45d051be4c0e23ca41659
SAJINASAJINA <shaleeshafi924@gmail.com>
2026-06-30 13:54:34 -0400

    Completed final project

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,61 +23,78 @@ public_users.post("/register", (req, res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/', async function (req, res) {
-    try {
-      const response = await axios.get("http://localhost:5000/");
-      return res.status(200).json(response.data);
-    } catch (err) {
-      return res.status(500).json({ message: err.message });
-    }
-  });
+// public_users.get('/', async function (req, res) {
+//     try {
+//       const response = await axios.get("http://127.0.0.1:5000");
+//       return res.status(200).json(response.data);
+//     } catch (err) {
+//       return res.status(500).json({ message: err.message });
+//     }
+//   });
+public_users.get('/', function (req,res) {
+    return res.status(200).json(books);
+});
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn', async function (req, res) {
-    const isbn = req.params.isbn;
+// public_users.get('/isbn/:isbn', async function (req, res) {
+//     const isbn = req.params.isbn;
   
-    try {
-      const response = await axios.get(`http://localhost:5000/isbn/${isbn}`);
-      return res.status(200).json(response.data);
-    } catch (err) {
-      return res.status(500).json({ message: err.message });
-    }
-  });
+//     try {
+//       const response = await axios.get(`http://127.0.0.1:5000/isbn/${isbn}`);
+//       return res.status(200).json(response.data);
+//     } catch (err) {
+//       return res.status(500).json({ message: err.message });
+//     }
+//   });
+public_users.get('/isbn/:isbn',
+function (req, res) {
+    const isbn = req.params.isbn;
+    return res.status(200).json(books[isbn]);
+});
   
 // Get book details based on author
-public_users.get('/author/:author', async function (req, res) {
-    const author = req.params.author;
+// public_users.get('/author/:author', async function (req, res) {
+//     const author = req.params.author;
   
-    try {
-      const response = await axios.get(`http://localhost:5000/author/${author}`);
-      return res.status(200).json(response.data);
-    } catch (err) {
-      return res.status(500).json({ message: err.message });
-    }
-  });
+//     try {
+//       const response = await axios.get(`http://127.0.0.1:5000/author/${author}`);
+//       return res.status(200).json(response.data);
+//     } catch (err) {
+//       return res.status(500).json({ message: err.message });
+//     }
+//   });
+public_users.get('/author/:author', function (req, res) {
+    const author = req.params.author;
+
+    const filteredBooks = Object.keys(books)
+        .filter(key => books[key].author === author)
+        .reduce((obj, key) => {
+            obj[key] = books[key];
+            return obj;
+        }, {});
+
+    return res.status(200).json(filteredBooks);
+});
+
 
 // Get all books based on title
-public_users.get('/title/:title', async function (req, res) {
+public_users.get('/title/:title', function (req, res) {
     const title = req.params.title;
-  
-    try {
-      const response = await axios.get(`http://localhost:5000/title/${title}`);
-      return res.status(200).json(response.data);
-    } catch (err) {
-      return res.status(500).json({ message: err.message });
-    }
-  });
+
+    const filteredBooks = Object.keys(books)
+        .filter(key => books[key].title === title)
+        .reduce((obj, key) => {
+            obj[key] = books[key];
+            return obj;
+        }, {});
+
+    return res.status(200).json(filteredBooks);
+});
 
 // Get book review
-public_users.get('/review/:isbn', async function (req, res) {
+public_users.get('/review/:isbn', function (req, res) {
     const isbn = req.params.isbn;
-  
-    try {
-      const response = await axios.get(`http://localhost:5000/review/${isbn}`);
-      return res.status(200).json(response.data);
-    } catch (err) {
-      return res.status(500).json({ message: err.message });
-    }
-  });
+    return res.status(200).json(books[isbn].reviews);
+});
 
 module.exports.general = public_users;
