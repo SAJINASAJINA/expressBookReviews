995246668ca93ad2d59e7cdeb0559e0248f61012
sajina <159539890+SAJINASAJINA@users.noreply.github.com>
2026-06-30 23:39:34 +0530

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,29 +23,11 @@ public_users.post("/register", (req, res) => {
 });
 
 // Get the book list available in the shop
-// public_users.get('/', async function (req, res) {
-//     try {
-//       const response = await axios.get("http://127.0.0.1:5000");
-//       return res.status(200).json(response.data);
-//     } catch (err) {
-//       return res.status(500).json({ message: err.message });
-//     }
-//   });
 public_users.get('/', function (req,res) {
     return res.status(200).json(books);
 });
 
 // Get book details based on ISBN
-// public_users.get('/isbn/:isbn', async function (req, res) {
-//     const isbn = req.params.isbn;
-  
-//     try {
-//       const response = await axios.get(`http://127.0.0.1:5000/isbn/${isbn}`);
-//       return res.status(200).json(response.data);
-//     } catch (err) {
-//       return res.status(500).json({ message: err.message });
-//     }
-//   });
 public_users.get('/isbn/:isbn',
 function (req, res) {
     const isbn = req.params.isbn;
@@ -53,16 +35,6 @@ function (req, res) {
 });
   
 // Get book details based on author
-// public_users.get('/author/:author', async function (req, res) {
-//     const author = req.params.author;
-  
-//     try {
-//       const response = await axios.get(`http://127.0.0.1:5000/author/${author}`);
-//       return res.status(200).json(response.data);
-//     } catch (err) {
-//       return res.status(500).json({ message: err.message });
-//     }
-//   });
 public_users.get('/author/:author', function (req, res) {
     const author = req.params.author;
 
